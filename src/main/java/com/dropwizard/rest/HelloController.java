78423b55ab7a4cec8c31f808158109c2ec2f1e6c
Anshum Gupta <gupta.anshum@flipkart.com>
2021-04-12 14:04:44 +0530

    drop wizard project

---
+++ src/main/java/com/dropwizard/rest/HelloController.java
@@ -0,0 +1,17 @@
+package com.dropwizard.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloController {
+
+    @GET
+    public String getEmployees() {
+        return "my dropwizard service";
+    }
+
+}
